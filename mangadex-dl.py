b89b856a16977219a7aee127efd0a9a9e0714040
hillmanation <jake.hillman.it@gmail.com>
2024-09-06 18:36:45 -0400

    init

---
+++ mangadex-dl.py
@@ -0,0 +1,65 @@
+import argparse
+import subprocess
+import sys
+import docker
+from src.manga_batch_downloader import MangaDownloader
+
+# Initialize client variable
+client = None
+
+
+def check_for_docker():
+    # Check if Docker is installed and running/enabled
+    global client  # GLobal variable to intitialize Docker client for later image check
+    try:
+        # Check if docker is installed first
+        subprocess.run(["docker", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # Check if it is running
+        subprocess.run(["docker", "info"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # Initialize a Docker client for image checking
+        client = docker.from_env()
+    except subprocess.CalledProcessError:
+        print("Docker is either not installed or not running, exiting...")
+        sys.exit(1)
+
+
+def check_and_pull_image(image_name):
+    # Check if the proper Docker image is available, and if not pull it
+    try:
+        client.images.get(image_name)
+    except docker.errors.ImageNotFound:
+        print(f"Docker image '{image_name}' not found locally. Pulling from Docker Hub...")
+        try:
+            # Pull from Docker Hub
+            client.images.pull(image_name)
+            print(f"Successfully pulled '{image_name}' from Docker Hub, moving on...")
+        except docker.errors.APIerror as e:
+            print(f"Error pulling image '{image_name}': {str(e)}")
+            sys.exit(1)
+
+
+def main():
+    # Check if Docker is installed
+    check_for_docker()
+
+    # Check for the image we need and pull it down if we don't have it
+    check_and_pull_image("mansuf/mangadex-downloader")
+
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Manga Downloader Script")
+    parser.add_argument('--export-dir', type=str, required=True, help="Local directory to save downloaded manga to.")
+    parser.add_argument('--manga-list', type=str, default='assets/manga-list.txt', help="Path to manga list file, the default is included with this repository.")
+    parser.add_argument('--max-containers', type=int, default=4, help="Maximum number of simultaneous containers to spin up to run the batch downloads. Use only as many containers as you're comfortable with. And probably less than the number of cores your machine has.")
+    args = parser.parse_args()
+
+    # Create a MangaDownloader instance with the provided export directory
+    downloader = MangaDownloader(
+        manga_list_file=args.manga_list,
+        export_dir=args.export_dir,
+        max_containers=args.max_containers
+    )
+    downloader.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/manga_batch_downloader.py
@@ -0,0 +1,94 @@
+import docker
+import time
+import re
+from datetime import datetime
+
+
+def valid_container_name(url):
+    # # Get just the last part of the url for the instance name
+    container_name = url.rsplit('/', 1)[-1]
+
+    # Oshi no Ko is weird, so it's url path ending started with a '-' which is bad juju
+    # so just in case let's filter for that here and ensure we start with an alphanumeric
+    if not re.match(r'^[a-zA-Z0-9]', container_name):  # Check if the string starts with a non-alphanumeric
+        # I could do some logic here and waste a lot of time, or we can just prepend something for the same effect
+        container_name = 'something' + container_name
+    return container_name
+
+
+class MangaDownloader:
+    def __init__(self, manga_list_file, export_dir, max_containers):
+        # Initialize Docker client
+        self.client = docker.from_env()
+        self.manga_list_file = manga_list_file
+        self.volume_mapping = export_dir
+        self.max_containers = max_containers
+        self.running_containers = []
+        self.defaults = "--no-group-name --use-chapter-title --delay-requests 1.5"
+
+    # Start download container instance
+    def start_download(self, instance_name, command_args):
+        # Start a Docker container for downloading manga from a url
+        try:
+            download_client = self.client.containers.run(
+                "mansuf/mangadex-downloader",  # Docker Image name
+                detach=True,  # Run in detached mode
+                name=instance_name,  # Name of Container instance
+                volumes={self.volume_mapping: {"bind": "/downloads", "mode": "rw"}},  # Local volume mapping
+                remove=True,  # Remove after container process stops
+                command=command_args
+            )
+            print(
+                f"Started container '{instance_name}' @ {datetime.now().strftime('%H:%M:%S')} with ID: {download_client.id}")
+            return instance_name  # Return for tracking
+        except docker.errors.APIError as e:
+            print(f"Error starting container '{instance_name}': {str(e)}")
+            return None
+
+    # Read in list of manga from a file
+    def read_manga_list(self):
+        manga_urls = []
+        try:
+            with open(self.manga_list_file, 'r') as f:
+                for line in f:
+                    # Strip any whitespace and append the url
+                    manga_urls.append(line.strip())
+        except FileNotFoundError:
+            print(f"Error: File '{self.manga_list_file}' not found.")
+        return manga_urls
+
+    def manage_containers(self):
+        # We're limiting the number of processes (containers) to 4 so we'll check to see how many containers are running
+        # here and only continue to the next url if the number of running containers is less than 4
+        while len(self.running_containers) >= self.max_containers:  # Check status of running containers
+            time.sleep(5)  # Wait 5 seconds before checking the containers again
+
+            for container_name in self.running_containers[:]:  # Iterate over a copy of the list
+                try:
+                    # If this returns a value the container is still running
+                    container = self.client.containers.get(container_name)
+                    # If we caught the container as it was shutting down before being removed
+                    if container.status == 'exited':
+                        print(f"Container '{container_name}' completed @ {datetime.now().strftime('%H:%M:%S')}")
+                        self.running_containers.remove(container_name)
+                except docker.errors.NotFound:
+                    # Container may have already been removed (we declared the '--rm' flag)
+                    self.running_containers.remove(container_name)
+                    print(f"Container '{container_name}' completed and removed @ {datetime.now().strftime('%H:%M:%S')}")
+
+    def run(self):
+        manga_list = self.read_manga_list()
+
+        if not manga_list:
+            print("No Urls provided in file.")
+            return
+        # Iterate through the list of manga and spin up to the max number of containers to download from at a time
+        for manga_url in manga_list:
+            container_name = valid_container_name(manga_url)
+            command_args = f"{manga_url} {self.defaults}"
+
+            name = self.start_download(container_name, command_args)
+            if name:
+                self.running_containers.append(name)
+
+            self.manage_containers()
